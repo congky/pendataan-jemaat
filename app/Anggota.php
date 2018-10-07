8417bc12359a00b39434e99022c25233994712dd
Congky <cokycongky@gmail.com>
2018-10-07 20:38:12 +0700

    disable autoincrement

--- app/Anggota.php
+++ app/Anggota.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Anggota extends Model
 {
 	protected $primaryKey = "no_anggota";
+    public $incrementing = false;
 	public $table = "t_anggota";
 	public $timestamps = false;
 }
--- app/Baptisan.php
+++ app/Baptisan.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Baptisan extends Model
 {
 	protected $primaryKey = "no_baptis";
+    public $incrementing = false;
 	public $table = "t_baptisan";
 	public $timestamps = false;
 }
