293b02194e45113e23ecfaa1b19bc58a07bf6cd8
congky <cokycongky@gmail.com>
2018-10-15 11:11:43 +0700

    anggota_kematian_id diubh kematian_id

--- resources/views/jemaat/baptisan.blade.php
+++ resources/views/jemaat/baptisan.blade.php
@@ -7,7 +7,7 @@
 
     <div class="box">
         <div class="box-header">
-            <h3 class="box-title">Data Table With Full Features</h3>
+            <h3 class="box-title">Data Baptisan</h3>
         </div>
         <!-- /.box-header -->
         <div class="box-body">
